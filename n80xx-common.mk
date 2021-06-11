b4bf14355ae87de72d459b9b05857aadb0e9d09f
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-06-11 07:02:50 +0200

    Copy ueventd.rc to vendor.

--- n80xx-common.mk
+++ n80xx-common.mk
@@ -28,6 +28,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/fstab.n80xx:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.smdk4x12 \
 	$(LOCAL_PATH)/rootdir/fstab.n80xx:$(TARGET_COPY_OUT_RAMDISK)/fstab.smdk4x12 \
     $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc\
     $(LOCAL_PATH)/rootdir/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
 
 # Audio
