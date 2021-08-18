d13ba109dc7d75e960100f78ea81afbe8394dec1
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-08-18 21:02:02 +0200

    init_n80xx.cpp: set rild.libargs only for n8000.

--- init/init_n80xx.cpp
+++ init/init_n80xx.cpp
@@ -76,7 +76,6 @@ void vendor_load_properties()
     std::string serial_number;
 
     property_override("ro.treble.enabled", "true");
-    property_override("rild.libargs", "-d /dev/ttyS0");
 
     if (ReadFileToString(serial_number_file, &serial_number)) {
         serial_number = Trim(serial_number);
@@ -90,6 +89,7 @@ void vendor_load_properties()
         property_override_dual("ro.build.description", "ro.vendor.build.description", "p4noterfxx-user 4.4.2 KOT49H N8000XXUDPB1 release-keys");
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/p4noterfxx/p4noterf:4.4.2/KOT49H/N8000XXUDPB1:user/release-keys");
         property_override("ro.build.product", "n8000");
+        property_override("rild.libargs", "-d /dev/ttyS0");
     } else if (bootloader.find("N8005") != std::string::npos) {
         property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-N8005");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "p4noterf");
