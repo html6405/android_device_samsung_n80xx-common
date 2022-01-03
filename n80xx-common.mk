6618984e8b43ae4226f9c77cad7c27f536198f84
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-01-03 15:23:54 +0100

    n80xx-common: move prebuilt apps to smdk4412-common.

--- n80xx-common.mk
+++ n80xx-common.mk
@@ -37,13 +37,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/tiny_hw.xml:vendor/etc/sound/n80xx
 
-
-# prebuild apps
-
-PRODUCT_PACKAGES += \
-	OpenCamera \
-	Via
-
 # sec_keyboard
 PRODUCT_PACKAGES += \
 	sec_keyboard \
